449c8d19f877d61b2866e14522748f5450858d43
serizawakagaya <a32441559@gmail.com>
2023-10-22 05:55:33 +0800

    基礎OK

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 var myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+myHeading.textContent = "SAKI!";
+
+/*var button1 = */
+document.querySelector("button").onclick = function () {
+    const music = new Audio('haoyay.mp3');
+    music.play();
+    myHeading.textContent = "SAKI!??!!";
+};
+/*document.querySelector("html").onclick = function () {
+    myHeading.textContent = "SAKI!??!!";
+    const music = new Audio('haoyay.mp3');
+    music.play();
+};*/
+/*button1.onclick = function () {
+    myHeading.textContent = "SAKI!??";
+    const music = new Audio('haoyay.mp3');
+    music.play();
+};*/
