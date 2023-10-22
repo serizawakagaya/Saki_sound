111d45593bcfccf5f2fa72e8c5bfc6eb276b5498
serizawakagaya <a32441559@gmail.com>
2023-10-23 01:43:56 +0800

    土法煉鋼法可行

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 var myHeading = document.querySelector("h1");
-myHeading.textContent = "SAKI!";
+
 
 /*var button1 = */
-document.querySelector("button").onclick = function () {
-    const music = new Audio('\src\sound\haoyay.mp3');
+document.querySelector("#好耶").onclick = function () {
+    const music = new Audio('../src/sound/haoyay.mp3');
+    music.volume=0.4;
     music.play();
-    myHeading.textContent = "SAKI!??!!";
+    myHeading.textContent = "SAKI!";
 };
-/*document.querySelector("html").onclick = function () {
-    myHeading.textContent = "SAKI!??!!";
-    const music = new Audio('haoyay.mp3');
+document.querySelector("#挖啊災").onclick = function () {
+    const music = new Audio('../src/sound/挖啊災.mp3');
+    music.volume=0.2;
     music.play();
-};*/
-/*button1.onclick = function () {
-    myHeading.textContent = "SAKI!??";
-    const music = new Audio('haoyay.mp3');
-    music.play();
-};*/
+    myHeading.textContent = "SAKI!?";
+};
